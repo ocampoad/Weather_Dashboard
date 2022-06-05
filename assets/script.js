79ef4204137c749308ac312566a2695af23cdb97
ocampoad <ocampo.adriane13@gmail.com>
2022-06-05 16:47:28 -0700

    able to store and get last city and data to local storage

--- assets/script.js
+++ assets/script.js
@@ -3,45 +3,66 @@ const currentDayEl = $(".currentDay")
 const currentDayStatsEl = $(".currentDayStats")
 const next5DaysEl = $(".next5Days")
 
-const weatherObj = {
-    "Clear": "<i class='fa-solid fa-sun'></i>",
-    "Clouds": "<i class='fa-solid fa-cloud'></i>",
-}
-
 function fetchFunccoordinates(url, cityValue) {
     fetch(url, {
         method: 'GET',
     })
         .then(response => response.json())
         .then(data => {
+            $("input").val("")
+            currentDayEl.text("")
+            next5DaysEl.text("")
             console.log('Success:', data);
+            if ($("button:contains(" + cityValue.toUpperCase() + ")").length <= 0) {
+                $("#searchArea").append("<button type='button' class=' btn-dark my-2 list-group-item list-group-item-action ' id = 'cityLists'  >" + cityValue.toUpperCase() + "</button>")
+            }
             let weatherIcon = data.current.weather[0].icon
-            console.log(weatherIcon)
             let tempinF = 1.8 * (data.current.temp - 273.15) + 32;
             let humidityVal = data.current.humidity
             let windSpeed = data.current.wind_speed
-            currentDayEl.append(cityValue + ": " + moment().format("dddd, MM/DD/YY") + " ")
+            let uvIndex = data.current.uvi
+            currentDayEl.append(cityValue.toUpperCase() + ": " + moment().format("dddd, MM/DD/YY") + " ")
             currentDayEl.append(" <img src = 'http://openweathermap.org/img/wn/" + weatherIcon + ".png'>")
             currentDayEl.append("<div class=' fw-semibold'> Temperature: " + tempinF.toPrecision(4) + " °F</div>")
             currentDayEl.append("<div class=' fw-semibold'> Humidity: " + humidityVal + "%</div>")
             currentDayEl.append("<div class=' fw-semibold'> Wind: " + windSpeed + " MPH</div>")
+            currentDayEl.append("<div class=' fw-semibold' id ='uvIndex'> UV Index: </div>")
+            if (uvIndex < 2) {
+                $("#uvIndex").append("<span class='badge text-bg-success '>" + uvIndex + "</span>")
+            } else if (uvIndex < 5) {
+                $("#uvIndex").append("<span class='badge text-bg-warning '>" + uvIndex + "</span>")
+            } else if (uvIndex < 8) {
+                $("#uvIndex").append("<span class='badge text-bg-danger '>" + uvIndex + "</span>")
+            } else {
+                $("#uvIndex").append("<span class='badge text-bg-dark '>" + uvIndex + "</span>")
+            }
             for (i = 1; i < 7; i++) {
                 let weatherIcondays = data.daily[i].weather[0].icon
-                console.log(weatherIcondays)
                 let tempinFdays = 1.8 * (data.daily[i].temp.day - 273.15) + 32;
                 let humidityValdays = data.daily[i].humidity
                 let windSpeeddays = data.daily[i].wind_speed
-                let fiveDaysCard = $("<div class=' col-2 fw-bold'></div>")
+                let fiveDaysCard = $("<div class=' col-2 fw-bold border-top border-secondary'></div>")
                 fiveDaysCard.append(moment().add(i, 'days').format("dddd") + "<img src = 'http://openweathermap.org/img/wn/" + weatherIcondays + ".png'></img>");
                 fiveDaysCard.append("<div class=' fw-semibold'> Temperature: " + tempinFdays.toPrecision(4) + " °F</div>")
                 fiveDaysCard.append("<div class=' fw-semibold'> Humidity: " + humidityValdays + "%</div>")
                 fiveDaysCard.append("<div class=' fw-semibold'> Wind: " + windSpeeddays + " MPH</div>")
                 next5DaysEl.append(fiveDaysCard)
             }
+            data.cityValue = cityValue
+            localStorage.setItem('cityWeatherData', JSON.stringify(data))
         })
         .catch((error) => {
             console.error('Error:', error);
         });
+}
+
+const lastInput = JSON.parse(localStorage.getItem("cityWeatherData"));
+if (lastInput != null) {
+    let lat = lastInput.lat;
+    let lon = lastInput.lon;
+    let cityValue = lastInput.cityValue;
+    const locationURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=351d28b4fbd0330fa3241a105d978dd6"
+    fetchFunccoordinates(locationURL, cityValue);
 }
 
 function fetchFuncCity(url, cityValue) {
@@ -57,36 +78,37 @@ function fetchFuncCity(url, cityValue) {
         })
         .catch((error) => {
             console.error('Error:', error);
+            alert("please input a valid city")
         });
 }
 
-$("button").click(
+$("#searchBtn").click(
     getCityValue);
 
-function getCityValue() {
-    let cityValue = $("input").val();
+$("#searchArea").click(
+    // function () {
+    //     alert("I clicked it")
+    // });
+    getCityListValue);
+
+function getCityListValue(event) {
+    let cityValue = $(event.target).text();
     if (cityValue) {
         const cityAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityValue + "&appid=351d28b4fbd0330fa3241a105d978dd6";
-        if ($("button:contains(" + cityValue + ")").length <= 0) {
-            $("#searchArea").append("<button type='button' class=' btn-dark my-2 list-group-item list-group-item-action ' id = 'cityLists'  >" + cityValue + "</button>")
-        }
         fetchFuncCity(cityAPI, cityValue)
-        $("input").val("")
-        currentDayEl.text("")
-        next5DaysEl.text("")
     } else {
         alert("please input a city")
     }
 
 };
 
-/* <div class="list-group">
-  <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-    The current button
-  </button>
-  <button type="button" class="list-group-item list-group-item-action">A second button item</button>
-  <button type="button" class="list-group-item list-group-item-action">A third button item</button>
-  <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
-  <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
-</div> */
-// ("<div class=' card col-1 fw-bold me-1'>" + moment().add(i, 'days').format("dddd") + "</div>")
+function getCityValue() {
+    let cityValue = $("input").val();
+    if (cityValue) {
+        const cityAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityValue + "&appid=351d28b4fbd0330fa3241a105d978dd6";
+        fetchFuncCity(cityAPI, cityValue)
+    } else {
+        alert("please input a city")
+    }
+
+};
